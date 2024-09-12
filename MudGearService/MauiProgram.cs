03ed2b78edc3a1c62043b855dfad4c904f5c2797
benjamin87-design <54944108+benjamin87-design@users.noreply.github.com>
2024-09-12 11:28:00 +0800

    customer CRUD error and success message handling with mudblazor

--- MudGearService/MauiProgram.cs
+++ MudGearService/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor;
 using MudBlazor.Services;
 
 namespace MudGearService
@@ -16,7 +17,18 @@ namespace MudGearService
                 });
 
             builder.Services.AddMauiBlazorWebView();
-            builder.Services.AddMudServices();
+            builder.Services.AddMudServices(config =>
+            {
+                config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+                config.SnackbarConfiguration.PreventDuplicates = false;
+                config.SnackbarConfiguration.NewestOnTop = false;
+                config.SnackbarConfiguration.ShowCloseIcon = true;
+                config.SnackbarConfiguration.VisibleStateDuration = 3000;
+                config.SnackbarConfiguration.HideTransitionDuration = 500;
+                config.SnackbarConfiguration.ShowTransitionDuration = 500;
+                config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            });
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
